f4247708a8588d55bc251216f816eb566533ca9f
Serhiy Shvayka <sergiyshvayka@gmail.com>
2022-09-12 14:53:43 +0300

    2

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,24 +4,24 @@ export default function Profile({ username, tag, location, avatar, stats }) {
   return (
     <div className={css.profile}>
       <div className={css.description}>
-        <img src={avatar} alt="User avatar" className={css.avatar} />
+        <img src={avatar} alt={username} className={css.avatar} />
         <p className={css.name}>{username}</p>
         <p className={css.tag}>@{tag}</p>
         <p className={css.location}>{location}</p>
       </div>
 
-      <ul className={css.stats}>
-        <li>
+      <ul className={css.stat}>
+        <li className={css.item}>
           <span className={css.label}>Followers</span>
-          <span className={css.quantity}>{stats.followers}</span>
+          <span className={css.views}>{stats.followers}</span>
         </li>
-        <li>
+        <li className={css.item}>
           <span className={css.label}>Views</span>
-          <span className={css.quantity}>{stats.views}</span>
+          <span className={css.views}>{stats.views}</span>
         </li>
-        <li>
+        <li className={css.item}>
           <span className={css.label}>Likes</span>
-          <span className={css.quantity}>{stats.likes}</span>
+          <span className={css.views}>{stats.likes}</span>
         </li>
       </ul>
     </div>
